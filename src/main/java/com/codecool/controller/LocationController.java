3b56c611af2c489aa67116dd5eed2da9b741134e
WojTechM <wojciech.m1997@gmail.com>
2018-10-01 14:31:28 +0200

    Implement LocationController - fields, contructor, get methods

--- src/main/java/com/codecool/controller/LocationController.java
+++ src/main/java/com/codecool/controller/LocationController.java
@@ -1,4 +1,33 @@
 package com.codecool.controller;
 
+import com.codecool.model.Location;
+import com.codecool.service.ILocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/location")
 public class LocationController {
+
+    private final ILocationService service;
+
+    @Autowired
+    public LocationController(ILocationService service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    List<Location> getAllLocations() {
+        return service.getAllLocations();
+    }
+
+    @GetMapping(path = "/{id}")
+    Location getLocationById(@PathVariable(value = "id") long id) {
+        return service.getLocationById(id);
+    }
 }
