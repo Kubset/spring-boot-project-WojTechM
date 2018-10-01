d39a43275b265cb42a3756e64583ee2293281ab2
Kubset <kuba1228334@gmail.com>
2018-10-01 13:31:02 +0200

    implement model excavation

--- src/main/java/com/codecool/model/Excavation.java
+++ src/main/java/com/codecool/model/Excavation.java
@@ -1,4 +1,41 @@
 package com.codecool.model;
 
 public class Excavation {
+
+    private long mineId;
+    private long resourceId;
+    private long excavation;
+
+    public Excavation() {
+    }
+
+    public Excavation(long mineId, long resourceId, long excavation) {
+        this.mineId = mineId;
+        this.resourceId = resourceId;
+        this.excavation = excavation;
+    }
+
+    public long getMineId() {
+        return mineId;
+    }
+
+    public void setMineId(long mineId) {
+        this.mineId = mineId;
+    }
+
+    public long getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(long resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public long getExcavation() {
+        return excavation;
+    }
+
+    public void setExcavation(long excavation) {
+        this.excavation = excavation;
+    }
 }
