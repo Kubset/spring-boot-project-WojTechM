479b7752eab752516521c5a67d85ece8af1a6413
WojTechM <wojciech.m1997@gmail.com>
2018-10-01 13:29:42 +0200

    Create Location model

--- src/main/java/com/codecool/model/Location.java
+++ src/main/java/com/codecool/model/Location.java
@@ -1,4 +1,51 @@
 package com.codecool.model;
 
 public class Location {
+
+    private long id;
+    private String name;
+    private String description;
+    private String address;
+
+    public Location() {
+    }
+
+    public Location(long id, String name, String description, String address) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
