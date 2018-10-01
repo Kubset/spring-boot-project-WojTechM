8e5337eb94d4209b0a39d65a1d80e43d3a8cf2b1
Kubset <kuba1228334@gmail.com>
2018-10-01 13:31:11 +0200

    implement model mine

--- src/main/java/com/codecool/model/Mine.java
+++ src/main/java/com/codecool/model/Mine.java
@@ -1,4 +1,44 @@
 package com.codecool.model;
 
+
+
 public class Mine {
+
+    private long id;
+    private long locationId;
+    private String name;
+
+    public Mine() {
+    }
+
+    public Mine(long id, long locationId, String name) {
+        this.id = id;
+        this.locationId = locationId;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(long locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
