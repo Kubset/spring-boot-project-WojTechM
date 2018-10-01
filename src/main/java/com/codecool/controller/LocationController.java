b58f1178465775230debc13deb5b385059d51fb4
WojTechM <wojciech.m1997@gmail.com>
2018-10-01 14:48:43 +0200

    Implement post method

--- src/main/java/com/codecool/controller/LocationController.java
+++ src/main/java/com/codecool/controller/LocationController.java
@@ -3,10 +3,7 @@ package com.codecool.controller;
 import com.codecool.model.Location;
 import com.codecool.service.ILocationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,11 @@ public class LocationController {
     @GetMapping(path = "/{id}")
     Location getLocationById(@PathVariable(value = "id") long id) {
         return service.getLocationById(id);
+    }
+
+    @PostMapping(consumes = "application/json")
+    void insertNewLocation(Location location) {
+        System.out.println(location.toString());
+        return;
     }
 }
