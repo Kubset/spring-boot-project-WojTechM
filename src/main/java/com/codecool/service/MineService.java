964157c064e539d1ac5151ceeda89d103f5afd11
Kubset <kuba1228334@gmail.com>
2018-10-02 11:54:36 +0200

    add error handling to insert/update methods

--- src/main/java/com/codecool/service/MineService.java
+++ src/main/java/com/codecool/service/MineService.java
@@ -1,6 +1,7 @@
 package com.codecool.service;
 
 import com.codecool.model.Mine;
+import com.codecool.repository.ILocationRepository;
 import com.codecool.repository.IMineRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,34 +10,57 @@ import java.util.List;
 @Service
 public class MineService implements IMineService {
 
-    private final IMineRepository repository;
+    private final IMineRepository mineRepository;
+    private final ILocationRepository locationRepository;
 
-    public MineService(IMineRepository repository) {
-        this.repository = repository;
+
+    public MineService(IMineRepository mineRepository, ILocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+        this.mineRepository = mineRepository;
     }
 
     public List<Mine> getAllMines() {
-        return repository.findAll();
+        return mineRepository.findAll();
     }
 
     @Override
     public Mine getMineById(long id) {
-        return repository.findById(id).orElse(null);
+        return mineRepository.findById(id).orElse(null);
     }
 
     @Override
-    public void deleteMine(long id) {
-        repository.deleteById(id);
+    public boolean deleteMine(long id) {
+        mineRepository.deleteById(id);
+
+        return false;
     }
 
     @Override
-    public void insertMine(Mine mine) {
-        repository.save(mine);
-
+    public boolean insertMine(Mine mine) {
+        if(mine.getId() == 0) {
+            return persistMine(mine);
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public void updateMine(Mine mine) {
-        repository.save(mine);
+    public boolean updateMine(Mine mine) {
+        if(mineRepository.findById(mine.getId()).isPresent()) {
+            return persistMine(mine);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean persistMine(Mine mine) {
+        long locationId = mine.getLocation().getId();
+
+        if(locationRepository.findById(locationId).isPresent()) {
+            mineRepository.save(mine);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
