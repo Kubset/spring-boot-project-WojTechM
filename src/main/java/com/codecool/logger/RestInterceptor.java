75ca4b5c8241cf4a6c59b1912db0abb09f9c47cd
WojTechM <wojciech.m1997@gmail.com>
2018-10-03 11:14:12 +0200

    Date of operation is now logged

--- src/main/java/com/codecool/logger/RestInterceptor.java
+++ src/main/java/com/codecool/logger/RestInterceptor.java
@@ -6,13 +6,17 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
 
 @Component
 public class RestInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        String message = String.format("%s: %s %s - %d",request.getRemoteAddr(), request.getMethod(),
+        String message = String.format("[%s] %s: %s %s - %d",
+                LocalDateTime.now().toString(),
+                request.getRemoteAddr(),
+                request.getMethod(),
                 request.getRequestURI(),
                 response.getStatus());
         App.log(message);
