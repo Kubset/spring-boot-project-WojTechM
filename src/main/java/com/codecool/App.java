12c73d571b4247605b961e4c76a9445a99efe13e
WojTechM <wojciech.m1997@gmail.com>
2018-10-04 10:14:49 +0200

    Create method responsible for adding sample data to database

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,9 +1,19 @@
 package com.codecool;
 
 
+import com.codecool.model.Excavation;
+import com.codecool.model.Location;
+import com.codecool.model.Mine;
+import com.codecool.model.Resource;
+import com.codecool.repository.IExcavationRepository;
+import com.codecool.repository.ILocationRepository;
+import com.codecool.repository.IMineRepository;
+import com.codecool.repository.IResourceRepository;
 import org.apache.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -18,4 +28,18 @@ public class App {
         log.info(message);
     }
 
+    @Bean
+    public CommandLineRunner demo(ILocationRepository locationRepository, IMineRepository mineRepository, IResourceRepository resourceRepository, IExcavationRepository excavationRepository) {
+        return (args) -> {
+            Location location = new Location("Sosnowiec", "Najgorsze miasto", "Nikt nie wie");
+            Mine mine = new Mine(location, "Sosnowiec Gadarka");
+            Resource resource = new Resource("Węgiel Brunatny", "Lipny, ale go sporo", 100000);
+            Excavation excavation = new Excavation(mine, resource, 10000);
+
+            locationRepository.save(location);
+            mineRepository.save(mine);
+            resourceRepository.save(resource);
+            excavationRepository.save(excavation);
+        };
+    }
 }
