3cb5b14937b3b11d3ee7d71ff094bec3a320e456
Kubset <kuba1228334@gmail.com>
2018-10-04 10:58:33 +0200

    override getById and deleteById methods in repository

--- src/main/java/com/codecool/repository/ILocationRepository.java
+++ src/main/java/com/codecool/repository/ILocationRepository.java
@@ -3,13 +3,21 @@ package com.codecool.repository;
 import com.codecool.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ILocationRepository extends JpaRepository<Location, Long> {
 
     @Query(value = "SELECT * FROM Location where is_archived = false", nativeQuery = true)
     public List<Location> findAll();
+
+    @Query(value = "SELECT * FROM Location where is_archived = false AND id = :id", nativeQuery = true)
+    public Optional<Location> findById(@Param("id") long id);
+
+    @Query(value = "UPDATE Location SET is_archived = true WHERE id = :id RETURNING *", nativeQuery = true)
+    public Optional<Location> deleteById(@Param("id") long id);
 }
--- src/main/java/com/codecool/repository/IResourceRepository.java
+++ src/main/java/com/codecool/repository/IResourceRepository.java
@@ -3,13 +3,21 @@ package com.codecool.repository;
 import com.codecool.model.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IResourceRepository  extends JpaRepository<Resource, Long> {
 
-    @Query(value = "SELECT * FROM Mine where is_archived = false", nativeQuery = true)
+    @Query(value = "SELECT * FROM Resource where is_archived = false", nativeQuery = true)
     public List<Resource> findAll();
+
+    @Query(value = "SELECT * FROM Resource where is_archived = false AND id = :id", nativeQuery = true)
+    public Optional<Resource> findById(@Param("id") long id);
+
+    @Query(value = "UPDATE Resourcce SET is_archived = true WHERE id = :id RETURNING *", nativeQuery = true)
+    public Optional<Resource> deleteById(@Param("id") long id);
 }
