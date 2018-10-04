87cbbf6a6eeff2ac7fa28f34f8e168a708d15408
Kubset <kuba1228334@gmail.com>
2018-10-04 13:21:01 +0200

    add email interceptor to config

--- src/main/java/com/codecool/logger/InterceptorAppConfig.java
+++ src/main/java/com/codecool/logger/InterceptorAppConfig.java
@@ -8,11 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Component
 public class InterceptorAppConfig  extends WebMvcConfigurerAdapter {
 
+    private final RestInterceptor restInterceptor;
+    private final EmailInterceptor emailInterceptor;
+
     @Autowired
-    RestInterceptor interceptor;
+    public InterceptorAppConfig(RestInterceptor restInterceptor, EmailInterceptor emailInterceptor) {
+        this.restInterceptor = restInterceptor;
+        this.emailInterceptor = emailInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor);
+        registry.addInterceptor(restInterceptor);
+        registry.addInterceptor(emailInterceptor);
     }
 }
